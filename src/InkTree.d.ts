613f3d3abf5671d1782e11e8d1b40fa286ac5148
furkle <furkle@furkleindustries.com>
2020-01-17 23:13:38 -0500

    Array typing fix.

--- src/InkTree.d.ts
+++ src/InkTree.d.ts
@@ -26,13 +26,13 @@ export interface InkTree {
   readonly iterationIndex: number;
   readonly namedContentVisits: ReadonlyArray<Record<string, number>>;
   readonly nodeMap: InkNodeMap;
-  readonly pathHistories: readonly InkPathHistory[][];
+  readonly pathHistories: readonly ReadonlyArray<InkPathHistory[]>;
 
   readonly getPrintoutFromContentItemHistory: (
     history: readonly InkNodeHistoryItem[],
   ) => Promise<string>;
 
   readonly queryNode: (id: string) => Promise<InkNode | null>;
-  readonly stateHistories: readonly InkStateHistory[][];
+  readonly stateHistories: readonly ReadonlyArray<InkStateHistory[]>;
   readonly tickCounts: readonly number[];
 }
